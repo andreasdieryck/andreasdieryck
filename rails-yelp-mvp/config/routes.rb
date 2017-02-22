94138a64b023ecc9fc5862b5843389c3b71e22c4
andreasdieryck <andreas.dieryck@gmail.com>
2017-02-22 15:53:14 +0100

    prepare rails project

--- rails-yelp-mvp/config/routes.rb
+++ rails-yelp-mvp/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'reviews/content'
+
+  get 'reviews/rating'
+
+  resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
